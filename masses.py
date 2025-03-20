9473b844f0c37200a5e5c819b91b98b9bfa3891e
iainv <iainvisa@gmail.com>
2025-03-20 18:40:52 +0100

    Average mass calculation

--- masses.py
+++ masses.py
@@ -11,26 +11,29 @@ print(monoisotopic)
 mass_dict = {}
 for unique_atom in set(monoisotopic["atom"]):
     subset = monoisotopic[monoisotopic["atom"] == unique_atom]
-    subset.sort_values(by=["abundance"], ascending=False, inplace=True)
+    subset = subset.sort_values(by=["abundance"], ascending=False)
     # print(subset)
     symbol = subset["symbol"].values[0].split("(")[0]
-    print(symbol)
+    #print(symbol)
 
     # symbol = list(subset.iterrows())[0]["symbol"].split("(")[0]
 
     # mass_dict[symbol] = {"abundant":subset.iloc[:,0]["mass"]}
     mass_dict[symbol] = {}
     for i, isotope in enumerate(subset.itertuples()):
-        print(isotope)
-        print(isotope.symbol)
+        #print(isotope)
+        #print(isotope.symbol)
         s = isotope.symbol.split("(")[1].split(")")[0]
         if i == 0:
             mass_dict[symbol]["abundant"] = float(isotope.mass)
+            mass_dict[symbol]["average"] = float(isotope.mass)*float(isotope.abundance)/100
+        else:
+            mass_dict[symbol]["average"] += float(isotope.mass) * float(isotope.abundance) / 100
         mass_dict[symbol][s] = float(isotope.mass)
-print(mass_dict.items())
+#print(mass_dict.items())
 
 
-def calculate_monoisotopic_mass(molecule=""):
+def calculate_monoisotopic_mass(molecule="", mode = "abundant"):
     mass = 0
     for i, a in enumerate(molecule):
         if a.isalpha() and a.upper() == a:
@@ -48,17 +51,29 @@ def calculate_monoisotopic_mass(molecule=""):
             if number == "":
                 number = 1
             number = int(number)
-            mass += mass_dict[atom]["abundant"] * int(number)
-            print(atom, number, mass_dict[atom]["abundant"], mass_dict[atom]["abundant"] * int(number))
+            try:
+                mass += mass_dict[atom][mode] * int(number)
+            except KeyError:
+                print("Atom not recognised:", atom)
+                return
+            print(atom, number, mass_dict[atom][mode], mass_dict[atom][mode] * int(number))
     print("Mass of {}: {}".format(molecule, mass))
     return mass
 
 
 calculate_monoisotopic_mass("C8H10N4O2")
 
+
+
+
 while True:
-    m = input()
-    calculate_monoisotopic_mass(m)
+    m = input("\nInput [MOLECULE] [abundant(default)/average]: \n>>").split(" ")
+    #print(m)
+    if len(m) > 1:
+        mode = m[1]
+    else:
+        mode = "abundant"
+    calculate_monoisotopic_mass(m[0], mode = mode)
 
 
 

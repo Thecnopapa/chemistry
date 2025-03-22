2304fef36b80fd1f1540e21b99a48d089d2cc779
Iain Visa <iainviss@gmail.com>
2025-03-22 11:17:11 +0000

    Implementes charge calculation on peaks more than one charge distant

--- esi.py
+++ esi.py
@@ -3,12 +3,12 @@
 
 
 
-H =1
+H =1.007276
 
 
-def get_ion_charge(peak1, peak2):
+def get_ion_charge(peak1, peak2, charge=1):
 
-    n= int(round((peak2-H) / (peak1-peak2),0))
+    n= int(round(charge*(peak2-H) / (peak1-peak2),0))
     print("The charges of the peaks are:", n, n+1)
     return n, n+1
     
@@ -23,7 +23,7 @@ def get_ion_mass(peak, charge):
     return mass
 
 
-peaks = (808.28, 771.75)
+peaks = (808.28, 739.20, 2)
 
 charges = get_ion_charge(*peaks)
 
@@ -33,4 +33,18 @@ print("Charges:", charges)
 print("Masses:", masses)
 print("Average mass:", sum(masses)/2)
 
+while True:
+    p= input("\n[Peak1 (-charge)], [Peak2 (+charge)]\n>>")
+    p = p.split(" ")
+    if len(p) >=3:
+        c=int(p[2])
+    else:
+        c=1
 
+    peaks = (float(p[0]), float(p[1]))
+    charges = get_ion_charge(*peaks, c)
+    masses = [get_ion_mass(p,c) for p,c in zip(peaks, charges)]
+    print("Peaks:", peaks)
+    print("Charges:", charges)
+    print("Masses:", masses)
+    print("Average mass:", sum(masses)/2)

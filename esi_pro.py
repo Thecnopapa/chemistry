28b644e4bab18269e262a2dcc2ac8f83db6fa674
iain <iainvisa@gmail.com>
2025-03-25 18:21:35 +0100

    ESI csv processing

--- esi_pro.py
+++ esi_pro.py
@@ -1,11 +1,21 @@
 import os
 import pandas as pd
 import numpy as np
+import regex as re
 
 df_path = "data/"+os.listdir("data")[0]
 
+arrayfile = []
+with open(df_path) as csvfile:
+    for l in csvfile:
+        a = l.replace("c(", "\"").replace(")", "\"")
+        a0 = a.split("\"")[-1]
+        a1 = a.split(",")[:-1]
+        arrayfile.append([b.replace("\"", "").replace("\n", "") for b in a1]+[a0])
+        print(arrayfile[-1])
 
-df = pd.read_csv(df_path, quotechar="\"")
+
+df = pd.DataFrame(arrayfile[1:], columns=arrayfile[0])
 
 print(df)
 
